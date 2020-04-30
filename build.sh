7b59bc6043a3efaf6a9d8d7e8c2749cd1873c647
elcymon <elcymon@gmail.com>
2020-04-30 12:33:31 +0100

    simplified non refactored build system

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 plugins_path=$PWD/compiled_plugins
-
-cd sources/plugins/build
+root_dir=$PWD
+cd $root_dir/sources/plugins/start_simulation/build
 cmake ../
 make
+cp -f start_simulation $plugins_path
 
-cp -f */*.so start_simulation/start_simulation $plugins_path
+cd $root_dir/sources/plugins/wp_swarm/build
+cmake ../
+make
+cp -f libwp_swarm.so $plugins_path
